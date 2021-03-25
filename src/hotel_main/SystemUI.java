6bf7fa563693ed2fb0edf26d4cb89b88f2563305
YenYuen <72780793+YenYuen@users.noreply.github.com>
2021-03-26 00:20:05 +0800

    Update SystemUI.java

--- src/hotel_main/SystemUI.java
+++ src/hotel_main/SystemUI.java
@@ -16,7 +16,7 @@ public class SystemUI {
 		this.scanner = scanner;
 	}
 
-	public void start() {
+	public void start() throws ParseException {
 
 		boolean exit = false;
 		do {
@@ -131,28 +131,8 @@ public class SystemUI {
 						
 
 	}
-	
-	public void updateBooking() {//hy
-		int choice;
-		do {
-			Booking theBooking = searchBooking();
-			System.out.println("Update Booking");
-			System.out.println("--------------");
-			System.out.println("1. Change booking detail.");
-			System.out.println("2. Cancel booking.");
-			System.out.println("3. Exit.");
-			System.out.print("Enter number ----> ");
-			choice = scanner.nextInt();
-			scanner.nextLine();
-			switch(choice) {
-			case 1:
-				changeBookingDets(theBooking);
-				break;
-			case 2: 
-				cancelBooking(theBooking);
-				break;
-			}
-		}while(choice != 3);
+
+	public void updateBooking() { // hy
 	}
 	
 	public void checkIn() { //april
@@ -200,8 +180,20 @@ public class SystemUI {
 		
 	}
 	
-	public void displayAvailableRoom(List<Room> availableRoomList) { // yy
-
+	private void displayAvailableRoom(List<Room> availableRoomList) { // yy
+		System.out.printf("%-9s", "Room ID");
+		System.out.printf("%-8s", "Rates");
+		System.out.printf("%-10s", "Discount");
+		System.out.printf("%-16s", "Number of Beds");
+		System.out.println("");
+		System.out.println("");
+		for(int i = 0; i < availableRoomList.size(); i++) {
+			System.out.printf("%-9d", availableRoomList.get(i).getRoomID());
+			System.out.printf("%-8.2f", availableRoomList.get(i).getRate());
+			System.out.printf("%-10.2f", availableRoomList.get(i).getDiscount());
+			System.out.printf("%-16d", availableRoomList.get(i).getNumOfBed());
+			System.out.println("");
+		}
 		
 	}
 
@@ -256,69 +248,62 @@ public class SystemUI {
 		System.out.println();
 	}
 
-	private void displayAvailableRoom(List<Room> availableRoomList) { // yy
+	private ClientProfile createClientProfile(String NRIC) { // yy
+		
+		String firstName;
+		String lastName;
+		String temp;
+		Gender gender = null;
+		String address;
+		
+		System.out.print("Enter client's first name ----> ");
+		firstName = scanner.nextLine();
+		
+		System.out.print("Enter client's last name ----> ");
+		lastName = scanner.nextLine();
+		
+		System.out.print("Enter client's gender(M/F) ----> ");
+		temp = scanner.nextLine();
+		if(temp.toLowerCase().equals("m")) {
+			gender = Gender.Male;
+		}
+		else if(temp.toLowerCase().equals("f")) {
+			gender = Gender.Female;
+		}
+		
+		System.out.print("Enter client's address ----> ");
+		address = scanner.nextLine();
+		
+		ClientProfile newClientProfile = new ClientProfile(NRIC, firstName, lastName, gender, address);
+		System.out.println("Client's profile is created.");
+		return newClientProfile;
 	}
 
-	private ClientProfile createClientProfile(String NRIC) { // yy
-		return null;
-	}
-	
-	public void changeBookingDets(Booking theBooking) { //hy
-		int choice, roomNo;
-		Date checkInDate, checkOutDate;
-		List<Room> availableRoomList;
-		Room room;
-		do {
-			System.out.println("Change Booking Detail");
-			System.out.println("---------------------");
-			System.out.println("1. Change check-in date & check-out date.");
-			System.out.println("2. Change room.");
-			System.out.println("3. Change number of guest.");
-			System.out.println("4. Exit.");
-			System.out.print("Enter number ----> ");
-			choice = scanner.nextInt();
-			scanner.nextLine();
-			switch(choice) {
-			case 1:
-				System.out.print("Enter new check-in date (DD/MM/YYYY) ----> ");
-				String checkInDateString = scanner.nextLine();
-				System.out.print("Enter new check-out date (DD/MM/YYYY) ----> ");
-				String checkOutDateString = scanner.nextLine();
-				checkInDate = new SimpleDateFormat("dd/MM/yyyy").parse(checkInDateString);
-				checkOutDate = new SimpleDateFormat("dd/MM/yyyy").parse(checkOutDateString);
-				availableRoomList = controller.findAvailableRoom(checkInDate, checkOutDate);
-				for(int i = 0; i < availableRoomList.size(); i++) {
-					Room arl = availableRoomList.get(i);
-					System.out.println("");
-				}
-				System.out.print("Enter new room ----> ");
-				roomNo = scanner.nextInt();
-				scanner.nextLine();
-				room = availableRoomList.get(roomNo - 1);
-				int numOfGuest = scanner.nextInt();
-				scanner.nextLine();
-				controller.updateBooking(theBooking, checkInDate, checkOutDate, room, numOfGuest);
-				break;
-			case 2: 
-				checkInDate = controller.getCheckInDate(theBooking);
-				checkOutDate = controller.getCheckOutDate(theBooking);
-				availableRoomList = controller.findAvailableRoom(checkInDate, checkOutDate);
-				for(int i = 0; i < availableRoomList.size(); i++) {
-					Room arl = availableRoomList.get(i);
-					System.out.println("");
-				}
-				System.out.print("Enter new room ----> ");
-				roomNo = scanner.nextInt();
-				scanner.nextLine();
-				room = availableRoomList.get(roomNo - 1);
-				//not complete
-				break;
-			}
-		}while(choice != 4);
+	private void changeBookingDets(Booking theBooking) { // hy
+
 	}
 
 	private void cancelBooking(Booking theBooking) { // yy
-
+		Calendar calendar = Calendar.getInstance();
+		Date dateToday = calendar.getTime();
+		boolean isRefundable = theBooking.validatePolicy(dateToday);
+		if(isRefundable) {
+			System.out.println("The deposit is refundable");
+			double deposit = theBooking.getPayment().getDeposit();
+			System.out.println("Deposit amount ----> " + deposit);
+			PaymentMethod paymentMethod = theBooking.getPayment().getPaymentMethod();
+			if(paymentMethod == PaymentMethod.CreditCard) {
+				System.out.println("Payment Method ----> Credit Card");
+				int cardNumber = theBooking.getPayment().getCardNumber();
+				System.out.println("Card Number ----> " + cardNumber);
+			}
+			else {
+				System.out.println("Payment Method ----> Cash");
+			}
+		}
+		theBooking.setStatus(BookingStatus.Cancelled);
+		System.out.println("The booking is cancelled.");
+		
 	}
 
 	private void makePayment(Booking theBooking) { // tbc
